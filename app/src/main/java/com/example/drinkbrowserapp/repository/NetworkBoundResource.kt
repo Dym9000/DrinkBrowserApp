dae26280a9b799231218af96923c3f8dcae48007
GrinPowerlifting <dom.grin9000@gmail.com>
2021-05-26 14:57:49 +0200

    Repository package updated: NetworkBoundResource

--- app/src/main/java/com/example/drinkbrowserapp/repository/NetworkBoundResource.kt
+++ app/src/main/java/com/example/drinkbrowserapp/repository/NetworkBoundResource.kt
@@ -1,7 +1,6 @@
 package com.example.drinkbrowserapp.repository
 
-import androidx.annotation.MainThread
-import androidx.annotation.WorkerThread
+
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MediatorLiveData
 import com.example.drinkbrowserapp.network.api.ApiEmptyResponse
@@ -9,6 +8,9 @@ import com.example.drinkbrowserapp.network.api.ApiErrorResponse
 import com.example.drinkbrowserapp.network.api.ApiSuccessResponse
 import com.example.drinkbrowserapp.network.api.GenericApiResponse
 import com.example.drinkbrowserapp.util.DataState
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 /**
  *Abstract class providing methods for network services and databases
@@ -20,85 +22,36 @@ abstract class NetworkBoundResource<RequestType, ResultType> {
 
     private val result = MediatorLiveData<DataState<ResultType>>()
 
-    init {
+    init{
         result.value = DataState.loading(null)
-        val dbSource = loadFromDb()
-        result.addSource(dbSource) { data ->
-            result.removeSource(dbSource)
-            if (shouldFetch(data)) {
-                fetchFromNetwork(dbSource)
-            } else {
-                result.addSource(dbSource) { newData ->
-                    setValue(DataState.success(newData))
-                }
+
+        GlobalScope.launch(IO){
+            val apiResponse = makeRequestCall()
+            result.addSource(apiResponse){
+                result.removeSource(apiResponse)
+                handleRequestCall(it)
             }
         }
+
     }
 
-    @MainThread
-    private fun setValue(newValue: DataState<ResultType>) {
-        if (result.value != newValue) {
-            result.value = newValue
+    private fun handleRequestCall(apiResponse: GenericApiResponse<RequestType>){
+        when (apiResponse){
+            is ApiSuccessResponse ->
+                onSuccessResponse(apiResponse)
+            is ApiEmptyResponse ->
+                onFailureResponse("HTTP 204. There was an empty response")
+            is ApiErrorResponse ->
+                onFailureResponse(apiResponse.errorMessage)
+            else -> onFailureResponse("ApiResponse not recognized.")
         }
     }
 
-    private fun fetchFromNetwork(dbSource: LiveData<ResultType>) {
-        val apiResponse = createCall()
-        // we re-attach dbSource as a new source, it will dispatch its latest value quickly
-        result.addSource(dbSource) { newData ->
-            setValue(DataState.loading(newData))
-        }
-        result.addSource(apiResponse) { response ->
-            result.removeSource(apiResponse)
-            result.removeSource(dbSource)
-            when (response) {
-                is ApiSuccessResponse -> {
-                    appExecutors.diskIO().execute {
-                        saveCallResult(processResponse(response))
-                        appExecutors.mainThread().execute {
-                            // we specially request a new live data,
-                            // otherwise we will get immediately last cached value,
-                            // which may not be updated with latest results received from network.
-                            result.addSource(loadFromDb()) { newData ->
-                                setValue(DataState.success(newData))
-                            }
-                        }
-                    }
-                }
-                is ApiEmptyResponse -> {
-                    appExecutors.mainThread().execute {
-                        // reload from disk whatever we had
-                        result.addSource(loadFromDb()) { newData ->
-                            setValue(DataState.success(newData))
-                        }
-                    }
-                }
-                is ApiErrorResponse -> {
-                    onFetchFailed()
-                    result.addSource(dbSource) { newData ->
-                        setValue(DataState.error(response.errorMessage, newData))
-                    }
-                }
-            }
-        }
-    }
+    abstract fun onFailureResponse(message: String)
 
-    protected open fun onFetchFailed() {}
+    abstract fun onSuccessResponse(response: GenericApiResponse<RequestType>)
 
-    fun asLiveData() = result as LiveData<DataState<ResultType>>
+    abstract suspend fun makeRequestCall():LiveData<GenericApiResponse<RequestType>>
 
-    @WorkerThread
-    protected open fun processResponse(response: ApiSuccessResponse<RequestType>) = response.body
-
-    @WorkerThread
-    protected abstract fun saveCallResult(item: RequestType)
-
-    @MainThread
-    protected abstract fun shouldFetch(data: ResultType?): Boolean
-
-    @MainThread
-    protected abstract fun loadFromDb(): LiveData<ResultType>
-
-    @MainThread
-    protected abstract fun createCall(): LiveData<GenericApiResponse<RequestType>>
+    fun returnAsLiveData() = result as LiveData<DataState<ResultType>>
 }
